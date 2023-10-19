d977fe1e464dca83139282f0eaa4056c4a3b976e
nordost8 <tokyonight6@gmail.com>
2023-10-19 21:31:27 +0200

    Commit: Remove extra code

--- translation_master.py
+++ translation_master.py
@@ -1,5 +1,6 @@
 import os
 import random
+import subprocess as sp
 import time
 
 from vidgear.gears import CamGear
@@ -7,7 +8,7 @@ from vidgear.gears import WriteGear
 
 from video_master import get_file_paths_and_names, PATH_TO_VIDEOS_FOLDER
 
-YOUTUBE_STREAM_KEY = 'eesv-9bje-4bfb-au40-7w7h' # or change to string, example: "5s63-hte6-baph-6h5b-7777"
+YOUTUBE_STREAM_KEY = 'eesv-9bje-4bfb-au40-7w7h'  # or change to string, example: "5s63-hte6-baph-6h5b-7777"
 
 
 class ShuffleCycle:
@@ -43,6 +44,7 @@ class YoutubeStreamer:
     writer = None
 
     def __init__(self):
+        self.video_paths_by_names = None
         self.output_params = {
             "-re": True,
             "-i": None,
@@ -66,25 +68,27 @@ class YoutubeStreamer:
 
         while True:
             self.actualize_playlist()
-            first_path = self.video_paths_by_names[self.video_queue[0]] if hasattr(self, 'video_paths_by_names') else None
-            if first_path is None:
+            if not hasattr(self, 'video_paths_by_names'):
                 print("Audio clips not found ;(\n Please add minimum 1 video with .mp4 format!")
             else:
                 break
             time.sleep(1)
 
-
-        self.writer = self.get_yt_writer(first_path)
-        # Odd code due to asynchrony:
-        # We write frames as stated in the documentation (this is mandatory)
-        # When the stream breaks - ValueError - we close the writer and continue the work of the farmer, who also sees
-        # that the writer died, and activates a new writer and starts passing the path of the next song to it
-        for frame, video_path in self.framer():
-            try:
-                self.writer.write(frame)
-            except ValueError:
-                self.writer.close()
-                self.writer = None
+        video_queue_cycled = ShuffleCycle(self.video_queue)
+        for video_name in video_queue_cycled:
+            video_path = self.video_paths_by_names[video_name]
+            writer = self.get_yt_writer(video_path)
+            stream = CamGear(source=video_path).start()
+            while True:
+                fframe = stream.read()
+                if fframe is None:
+                    stream.stop()
+                    break
+                try:
+                    writer.write(fframe)
+                except ValueError as e:
+                    print(f"The stream for {video_path} was interrupted at the expected location:", e)
+                    break
 
     def actualize_playlist(self):
         actual_videos = get_file_paths_and_names(PATH_TO_VIDEOS_FOLDER, '.mp4')
@@ -92,24 +96,7 @@ class YoutubeStreamer:
             self.video_paths_by_names = actual_videos
             new_videos = [video_name for video_name, video_path in actual_videos.items() if
                           video_name not in self.video_queue]
-            if not self.video_queue:
-                # if first:
-                random.shuffle(new_videos)
             self.video_queue += new_videos
-
-    def framer(self):
-        video_queue_cycled = ShuffleCycle(self.video_queue)
-        for video_name in video_queue_cycled:
-            self.actualize_playlist()
-            video_path = self.video_paths_by_names[video_name]
-            stream = CamGear(source=video_path).start()
-            while True:
-                fframe = stream.read()
-                print('frame:', str(fframe)[:10])
-                if not self.writer or fframe is None:
-                    stream.stop()
-                    break
-                yield fframe, video_path
 
     def get_yt_writer(self, video_path):
         self.output_params['-i'] = video_path
