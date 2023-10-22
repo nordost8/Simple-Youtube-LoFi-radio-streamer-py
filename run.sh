db3064a56a90d0859c5bd5898e232aaf3d752e15
nordost8 <tokyonight6@gmail.com>
2023-10-22 23:15:47 +0200

    Commit: Changes in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-nohup -u python video_master.py > video_master_log.txt 2>&1 &
-
-nohup -u python translation_master.py > translation_master_log.txt 2>&1 &
+nohup python video_master.py > video_master_log.txt 2>&1 &
+nohup python translation_master.py > translation_master_log.txt 2>&1 &
